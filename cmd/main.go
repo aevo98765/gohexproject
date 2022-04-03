35989d34096544387842588056f51454c70855eb
Ashley Evans <ashley.evans@md.catapult.org.uk>
2022-04-03 23:53:27 +0100

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"gohexproject/internal/adapters/core/arithmetic"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	arithAdaptor := arithemtic.NewAdapter()
+	arithAdaptor.Addition()
 }
