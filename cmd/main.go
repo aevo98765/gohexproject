945c5103385a75ed60c81ccd666c4c12316821a4
Ashley Evans <ashley.evans@md.catapult.org.uk>
2022-04-04 10:19:19 +0100

    import internal/adapters/core/arithmetic

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
-	"gohexproject/internal/adapters/core/arithmetic"
+	"hex/internal/adapters/core/arithmetic"
 )
 
 func main() {
 	fmt.Println("Hello World")
-	arithAdaptor := arithemtic.NewAdapter()
-	arithAdaptor.Addition()
+	arithAdaptor := arithmetic.NewAdapter()
+	additionResult, error := arithAdaptor.Addition(2, 4)
+	if error != nil {
+		fmt.Println(additionResult)
+	}
+	println(error)
+	
+	// arithAdaptor.Subtraction()
+	// arithAdaptor.Multiplication()
+	// arithAdaptor.Division()
 }
